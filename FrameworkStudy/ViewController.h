7598b0f0bc80c13a87076e4607712ab9d75a2353
sinjinu <jinu712@gmail.com>
2022-09-23 15:34:24 +0900

    Initial Commit

---
+++ FrameworkStudy/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  FrameworkStudy
+//
+//  Created by 신진우 on 2022/09/23.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FrameworkStudy/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  FrameworkStudy
+//
+//  Created by 신진우 on 2022/09/23.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
